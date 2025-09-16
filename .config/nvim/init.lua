5e66d9a16877716e1cc4f8f83aa24d0fa8f619ef
Daniel Paiva <daniel.paiva@theta.co.nz>
2025-09-16 14:57:43 +1200

    added-tmux-nvim-git-ignore

---
+++ .config/nvim/init.lua
@@ -0,0 +1,28 @@
+-- Basic options migrated from init.vim
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+-- Keymaps migrated from init.vim
+vim.keymap.set("n", "ff", "<cmd>Telescope find_files<cr>", { silent = true })
+vim.keymap.set("n", "fg", "<cmd>Telescope live_grep<cr>", { silent = true })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { silent = true })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { silent = true })
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup plugins (defined in lua/plugins)
+require("lazy").setup("plugins")
+
