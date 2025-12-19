2f11b627310af64728370f2a871460300bf3d1db
Daniel Paiva <daniel.paiva@theta.co.nz>
2025-12-19 14:36:07 +1300

    added-.config-files-bash-zsh-nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,16 @@
--- Basic options migrated from init.vim
+-- Basic options 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.g.autoformat = false
+vim.opt.autochdir = true
 
--- Keymaps migrated from init.vim
+-- Keymaps 
 vim.keymap.set("n", "ff", "<cmd>Telescope find_files<cr>", { silent = true })
 vim.keymap.set("n", "fg", "<cmd>Telescope live_grep<cr>", { silent = true })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { silent = true })
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { silent = true })
+
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -22,7 +25,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+vim.opt.winfixbuf = false
 
 -- Setup plugins (defined in lua/plugins)
 require("lazy").setup("plugins")
-
