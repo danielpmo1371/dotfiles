8b79e9a0b13fbdd93e9e7a0aabf975d3fc13c1a3
Daniel Paiva <daniel.p@rushdigital.co.nz>
2025-12-21 15:38:53 +1300

    moved keymaps from main init.lua to keymaps file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,13 +5,6 @@ vim.opt.clipboard = "unnamedplus"
 vim.g.autoformat = false
 vim.opt.autochdir = true
 
--- Keymaps 
-vim.keymap.set("n", "ff", "<cmd>Telescope find_files<cr>", { silent = true })
-vim.keymap.set("n", "fg", "<cmd>Telescope live_grep<cr>", { silent = true })
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { silent = true })
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { silent = true })
-
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,7 @@
+-- Keymaps 
 vim.keymap.set("n", "<leader>X", "<cmd>LazyExtras<cr>", { desc = "LazyExtras" })
+
+vim.keymap.set("n", "ff", "<cmd>Telescope find_files<cr>", { silent = true })
+vim.keymap.set("n", "fg", "<cmd>Telescope live_grep<cr>", { silent = true })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { silent = true })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { silent = true })
