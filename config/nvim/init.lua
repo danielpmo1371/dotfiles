dd39acbddbc7f08e78333f0550e1d84b7944bba9
Daniel Paiva <daniel.p@rushdigital.co.nz>
2025-12-21 16:40:14 +1300

    moved nvim options to options file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,24 +1,16 @@
--- Basic options 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
-vim.g.autoformat = false
-vim.opt.autochdir = true
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-vim.opt.winfixbuf = false
 
 -- Setup plugins (defined in lua/plugins)
 require("lazy").setup({
