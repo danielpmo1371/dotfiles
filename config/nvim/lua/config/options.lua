7bc2165d7ad06effaab7bafc62b591ca47d829ec
Daniel Paiva <daniel.p@rushdigital.co.nz>
2025-12-21 16:40:14 +1300

    moved nvim options to options file

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- See :help vim.opt and :help vim.g
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.g.autoformat = false
+vim.opt.autochdir = true
+vim.opt.winfixbuf = false
