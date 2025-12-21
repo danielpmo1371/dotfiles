00d8121057c69bdc9199e4733d27ec5c12222088
Daniel Paiva <daniel.p@rushdigital.co.nz>
2025-12-21 15:39:34 +1300

    removed avante from plugins file

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,61 +1,39 @@
 return {
-  -- Telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 
-  -- Treesitter for syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local ts = require("nvim-treesitter")
-      ts.setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "bash", "python", "javascript", "typescript" },
-        auto_install = true,
-      })
-      -- Enable highlighting via vim.treesitter (new API)
-      vim.treesitter.language.register("bash", "sh")
-    end,
-  },
+	-- Treesitter for syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter")
+			ts.setup({
+				ensure_installed = { "lua", "vim", "vimdoc", "bash", "python", "javascript", "typescript" },
+				auto_install = true,
+			})
+			-- Enable highlighting via vim.treesitter (new API)
+			vim.treesitter.language.register("bash", "sh")
+		end,
+	},
 
-  -- Avante and related deps
-  {
-    "yetone/avante.nvim",
-    branch = "main",
-    build = "make",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "MeanderingProgrammer/render-markdown.nvim",
-      { "hrsh7th/nvim-cmp",          optional = true },
-      { "nvim-tree/nvim-web-devicons", optional = true },
-      { "HakonHarnes/img-clip.nvim", optional = true },
-      { "zbirenbaum/copilot.lua",    optional = true },
-      { "stevearc/dressing.nvim",    optional = true },
-      { "folke/snacks.nvim",         optional = true },
-    },
-    config = function()
-      require("avante").setup({})
-    end,
-  },
-
-  -- Individual plugins from your previous config
-  { "hashivim/vim-terraform" },
-  { "mg979/vim-visual-multi", branch = "master" },
-  { "nvim-lua/plenary.nvim" },
-  { "MunifTanjim/nui.nvim" },
-  { "MeanderingProgrammer/render-markdown.nvim" },
-  { "hrsh7th/nvim-cmp" },
-  { "nvim-tree/nvim-web-devicons" },
-  { "HakonHarnes/img-clip.nvim" },
-  { "zbirenbaum/copilot.lua" },
-  { "stevearc/dressing.nvim" },
-  { "folke/snacks.nvim" },
+	-- Individual plugins from your previous config
+	{ "hashivim/vim-terraform" },
+	{ "mg979/vim-visual-multi", branch = "master" },
+	{ "nvim-lua/plenary.nvim" },
+	{ "MunifTanjim/nui.nvim" },
+	{ "MeanderingProgrammer/render-markdown.nvim" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "HakonHarnes/img-clip.nvim" },
+	{ "zbirenbaum/copilot.lua" },
+	{ "stevearc/dressing.nvim" },
+	{ "folke/snacks.nvim" },
 }
-
