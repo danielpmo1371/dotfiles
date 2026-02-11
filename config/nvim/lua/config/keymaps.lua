7f89017b1f5ba44bacf66d9771383af102e61ac7
Daniel Paiva <danielpmo@gmail.com>
2026-02-12 11:08:18 +1300

    nvim tab cycle buffers shit-tab cycles windows

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "ff", "<cmd>Telescope find_files<cr>", { silent = true })
 vim.keymap.set("n", "fg", "<cmd>Telescope live_grep<cr>", { silent = true })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { silent = true })
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { silent = true })
+
+-- Cycle buffers with Tab, cycle windows with Shift+Tab
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<C-w>w", { desc = "Next window" })
